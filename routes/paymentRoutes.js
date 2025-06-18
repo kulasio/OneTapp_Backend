1d8543d736b2b0055c168aa18342714226a5e1fd
kulasio <148292561+kulasio@users.noreply.github.com>
2025-06-18 20:45:43 +0800

    payment

--- routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-
-// Route to create Maya checkout
-router.post('/create', paymentController.createCheckout);
-
-module.exports = router; 
