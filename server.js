9488450845206bbd2cd18cd1e29001390e21c3aa
kulasio <148292561+kulasio@users.noreply.github.com>
2025-06-18 19:40:05 +0800

    Payment

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://one-tapp-frontend.vercel.app'
+}));
 
 // Routes
 // app.use('/api/users', require('./routes/userRoutes'));
