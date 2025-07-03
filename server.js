a07a0eaa97816e0918e447641f138ac86840e1b8
kulasio <148292561+kulasio@users.noreply.github.com>
2025-07-03 16:05:31 +0800

    cards

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: 'https://one-tapp-frontend.vercel.app'
+  origin: [
+    'https://one-tapp-frontend.vercel.app',
+    'https://nfc-card-1.vercel.app'
+  ]
 }));
 
 // Routes
